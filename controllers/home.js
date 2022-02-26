c43e7221dac26b20ee94a2dc68c7ea58264213f3
Kiki <kirild1997@gmail.com>
2022-02-26 08:33:57 +0200

    catalog, edit, delete,  full app

--- controllers/home.js
+++ controllers/home.js
@@ -1,7 +1,32 @@
+const { getPosts, getPostById } = require('../services/postService.js');
+const { postViewModel } = require('../util/mappers.js');
+
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
     res.render('home', { title: 'Home Page' });
-})
+});
+
+
+router.get('/catalog', async (req, res) => {
+    const posts = (await getPosts()).map(postViewModel);
+    res.render('catalog', { title: 'Catalog', posts });
+});
+
+
+router.get('/catalog/:id', async (req, res) => {
+    const id = req.params.id;
+    const post = postViewModel(await getPostById(id));
+
+    if (req.session.user) {
+        post.hasUser = true;
+        if (req.session.user._id == post.author._id) {
+            post.isAuthor = true;
+        }
+    }
+
+    res.render('details', { title: post.title, post })
+});
+
 
 module.exports = router;
